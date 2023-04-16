9c8888b8e231e89d34eff25c899156bcccf4202c
RomanACU <alexciur86@gmail.com>
2023-04-16 02:09:51 -0600

    Ciclo FOR y metodo push para crear array

---
+++ Array/cicloFOR.js
@@ -0,0 +1,9 @@
+//Crear arreglo de numeros con "push" utilizando un ciclo FOR
+let Numeros=[];
+
+for(i=0; i<=25; i++){
+   Numeros.push(i);
+};
+
+console.log(Numeros);
+console.log(Numeros.splice(3,5));
